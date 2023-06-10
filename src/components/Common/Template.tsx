59dda23ca85492e09bc05068a80808ee991e5bc1
Geuni <dlrmsgnl620@naver.com>
2023-06-10 18:35:46 +0900

    Add: favicon

--- src/components/Common/Template.tsx
+++ src/components/Common/Template.tsx
@@ -54,21 +54,21 @@ const Template: FunctionComponent<TemplateProps> = function ({
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
+          href="assets/favicon/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon/favicon-32x32.png"
+          href="assets/favicon/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon/favicon-16x16.png"
+          href="assets/favicon/favicon-16x16.png"
         />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="manifest" href="assets/favicon/site.webmanifest" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff"></meta>
 
